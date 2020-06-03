403034702a59703730ca171a9656007ad91eff61
Sai Charan <tsaicharan03@gmail.com>
2020-06-02 21:19:02 -0500

    PatientRecords has been created

---
+++ app/src/main/java/app/patientocity/PatientRecords.java
@@ -0,0 +1,14 @@
+package app.patientocity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PatientRecords extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_records);
+    }
+}
