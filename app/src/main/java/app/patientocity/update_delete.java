56096573fb5c8a0e4fa742343064435995b3e103
Rachael Chen <rayton.chen@gmail.com>
2020-06-06 11:41:45 -0400

    stuff

---
+++ app/src/main/java/app/patientocity/update_delete.java
@@ -0,0 +1,17 @@
+package app.patientocity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class update_delete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_delete);
+
+        public void btnUpdate_Click(View view) {
+        }    }
+}
